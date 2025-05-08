50f860d85b62a4fdfda80c628d828db44f3468e6
diksha sinha <diksha.sinha2003@gmail.com>
2025-05-09 01:37:33 +0530

    9th May 2025

---
+++ brand-page/src/Components/Hero.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+export const Hero = () => {
+  return (
+    <div className="heroSection">
+      <div>
+        <div className="heading">YOUR FEET DESERVE THE BEST</div>
+        <p>
+          YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+          SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+          SHOES.
+        </p>
+        <div className="btnContainer">
+          <button>Shop Now</button>
+          <button className="catBtn">Category</button>
+        </div>
+        <div>Also available on</div>
+        <div>
+          <img />
+          <img />
+        </div>
+      </div>
+      <div>
+        <img src="./shoe_image.png" />
+      </div>
+    </div>
+  );
+};
---
+++ brand-page/src/Components/Navigation.jsx
@@ -0,0 +1,19 @@
+const Navigation = () => {
+  return (
+    <nav className="nav">
+      <div className="logo">
+        <img src="brand_logo.png" alt="brand-logo" />
+      </div>
+      <ul>
+        <li href="#">MENU</li>
+        <li href="#">LOCATION</li>
+        <li href="#">ABOUT</li>
+        <li href="#">CONTACT</li>
+      </ul>
+
+      <button>Login</button>
+    </nav>
+  );
+};
+
+export default Navigation;
